c6eae8d61f80f1a7ede3017980a548475e00c95f
elevenstand7 <elevenstand7@gmail.com>
2023-09-07 14:16:55 -0400

    finished pokemon model

--- practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/db/schema.rb
+++ practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/db/schema.rb
@@ -37,7 +37,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_07_175322) do
     t.bigint "pokemon_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["move_id"], name: "index_poke_moves_on_move_id"
     t.index ["pokemon_id", "move_id"], name: "index_poke_moves_on_pokemon_id_and_move_id", unique: true
     t.index ["pokemon_id"], name: "index_poke_moves_on_pokemon_id"
   end
