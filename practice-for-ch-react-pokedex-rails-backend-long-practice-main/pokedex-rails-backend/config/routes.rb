b427ca1b7b1a1841eebef047c7fd2f10494d4e42
elevenstand7 <elevenstand7@gmail.com>
2023-09-07 15:45:35 -0400

    working on ph2

--- practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/config/routes.rb
+++ practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api, defaults: { format: :json } do
+    # define routes here to include them in the api namespace
+      resources :pokemon, only: [:create, :index, :show, :update] do
+        # get ‘types’, on: :collection
+        resources :items, only: [:index, :create, :update, :destroy], shallow: true
+      end
+  end
 end
